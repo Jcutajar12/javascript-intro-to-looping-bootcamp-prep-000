67b97bcc4c3f110ad7370ec9ff612aaa4946e92b
Joe Cutajar <jcutajar12@gmail.com>
2017-04-20 16:12:40 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+
+
+function forLoop (array) {
+    for(i = 0; i < 25; i++) {
+      if (i == 1){
+        array.push("i am 1 strange loop");
+      }
+      else{
+        array.push('I am ${i} strange loops.');
+      }
+    }
+}
