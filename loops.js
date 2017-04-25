52e5078fb18634faf3356ebe6ad0b786ebc8e609
Joe Cutajar <jcutajar12@gmail.com>
2017-04-25 17:53:28 +0000

    Done.

--- loops.js
+++ loops.js
@@ -8,5 +8,30 @@ function forLoop (array) {
       else{
         array.push('I am ${i} strange loops.');
       }
+
     }
+    return array;
+}
+
+function whileLoop(n) {
+
+  while (n > 0) {
+    console.log(n--);
+  }
+
+return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  
+do {
+  console.log(array.pop());
+}
+while(array.length > 0 && maybeTrue());
+
+return array;
 }
